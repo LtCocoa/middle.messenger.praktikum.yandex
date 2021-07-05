26d0924ab4d0e8ab3ee5f12ab552ab9aefd95951
Дементьев Денис Геннадьевич <ddementyev@nic.etu>
2021-07-05 17:46:48 +0300

    profile page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import authTemplate from './pages/auth/auth.tmpl';
 import registerTemplate from './pages/registration/registration.tmpl';
 import profileTemplate from './pages/profile/profile.tmpl';
-import './index.css';
+import './index.scss';
 
 const root = document.querySelector('#root');
 root.innerHTML = profileTemplate;
--- src/pages/profile/profile.tmpl.js
+++ src/pages/profile/profile.tmpl.js
@@ -1,8 +1,40 @@
 import Templator from '../../templator/Templator';
+import './profile.scss';
 
 const template = `
   <main class="profile">
-    
+    <h1 class="profile__name">Имя</h1>
+    <div class="profile-info">
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Почта</label>
+            <input class="profile-info-field__input" type="email" name="email" />
+        </div>
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Логин</label>
+            <input class="profile-info-field__input" type="text" name="login" />
+        </div>
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Имя</label>
+            <input class="profile-info-field__input" type="text" name="first_name" />
+        </div>
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Фамилия</label>
+            <input class="profile-info-field__input" type="text" name="second_name" />
+        </div>
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Имя в чате</label>
+            <input class="profile-info-field__input" type="text" name="display_name" />
+        </div>
+        <div class="profile-info-field">
+            <label class="profile-info-field__label">Телефон</label>
+            <input class="profile-info-field__input" type="tel" name="phone" />
+        </div>
+    </div>
+    <div class="profile-controls">
+        <span class="profile-controls__button">Изменить данные</span>
+        <span class="profile-controls__button">Изменить пароль</span>
+        <span class="profile-controls__button red">Выйти</span>
+    </div>
   </main>
 `;
 
